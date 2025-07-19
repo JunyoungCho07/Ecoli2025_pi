4b62fb3b21c08dab112b35c50b52a165d2e29743
JY <chojunyoung070523@gmail.com>
2025-07-19 20:27:49 +0900

    modified:   test_env.py

--- test_env.py
+++ test_env.py
@@ -1,10 +1,11 @@
 # test_env.py
 from PIL import Image, ImageTk
+# import picamera2
 from picamera2 import Picamera2
 import board, neopixel
 
 print("Pillow:", Image.__version__)
-print("Camera:", Picamera2.version)
+# print("Camera:", picamera2.__version__) #dosen't work
 pixels = neopixel.NeoPixel(board.D18, 8, auto_write=False)
 pixels.fill((0, 50, 0)); pixels.show()
 print("NeoPixel OK")
